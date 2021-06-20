e3e1bd577b341c9c08e618c356bdb4f698167ead
Jordi <jcaballol94@gmail.com>
2021-06-20 15:27:12 +0100

    Better wind

--- Assets/Scripts/Vegetation/VegetationUtils.hlsl
+++ Assets/Scripts/Vegetation/VegetationUtils.hlsl
@@ -1,6 +1,8 @@
 #ifndef VEGETATION_UTILS_HLSL
 #define VEGETATION_UTILS_HLSL
 
+#include "noise.hlsl"
+
 void Billboard_float(in float3 InPos, in float3 ViewDir, out float3 OutPos)
 {
 	// Get the flat view dir (reversed)
@@ -18,6 +20,37 @@ void Billboard_float(in float3 InPos, in float3 ViewDir, out float3 OutPos)
 	rotMatrix._m11 = rotMatrix._m33 = 1;
 
 	OutPos = mul(rotMatrix, float4(InPos, 1));
+}
+
+void SmallWind_float(in float3 WorldPivot, in float WindAmplitude, in float WindFrequency, in float Time, out float2 Wind)
+{
+	float2 noiseUv = WorldPivot.xz;
+
+	// Get a random direction for this
+	float3 dir = 0;
+	dir.xz = normalize(rand_2_10(noiseUv));
+
+	// Get a random angle based on the sinus
+	float time = rand_1_05(noiseUv);
+	time += Time;
+	time *= WindFrequency;
+	time = frac(time) * 360;
+	float angle = WindAmplitude * sin(radians(time));
+
+	// Return
+	Wind = dir.xz * angle;
+}
+
+void ApplyWind_float(in float3 Pos, in float Height, in float2 Wind, out float3 OutPos)
+{
+	// Extract the data
+	float angle = length(Wind);
+	float2 direction = angle > 0 ? Wind / angle : 0;
+	angle = radians(angle);
+
+	OutPos = float3(0,0,0);
+	OutPos.xz = Pos.xz + Height * direction * sin(angle);
+	OutPos.y = Height * cos(angle);
 }
 
 #endif //VEGETATION_UTILS_HLSL
